d422f459ce6a017bb5ec29ea42882d0338f9dadc
Chawana Maseka <chawana.maseka@gmail.com>
2025-11-17 10:17:19 +0200

    build landing page

---
+++ src/unAuth/LandingPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './LandingPage.css';
+
+const LandingPage = () => {
+  return (
+    <div className="landing-page">
+      <div className="content">
+        <h1>Hello Landing Page</h1>
+        <p>Welcome to your new landing page!</p>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
+
